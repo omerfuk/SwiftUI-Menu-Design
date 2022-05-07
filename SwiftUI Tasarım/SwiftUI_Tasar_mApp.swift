3dae962559dd8713cfd0661c14bcaebbec716398
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-07 23:08:11 +0300

    Initial Commit

---
+++ SwiftUI Tasarım/SwiftUI_Tasar_mApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tasar_mApp.swift
+//  SwiftUI Tasarım
+//
+//  Created by Ömer Faruk Kılıçaslan on 7.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tasar_mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
