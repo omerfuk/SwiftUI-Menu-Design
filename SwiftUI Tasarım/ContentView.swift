beca3893ad711468c46af9bbbe02472212e5ac59
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-07 23:18:00 +0300

    .

--- SwiftUI Tasarım/ContentView.swift
+++ SwiftUI Tasarım/ContentView.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader{ geometry in
+            VStack{
+                Image("yemekresim").resizable().frame(width:geometry.size.width,height: geometry.size.height / 3)
+                
+                
+                VStack(alignment:.leading){
+                    Text("Köfte")
+                        .font(.title)
+                        .foregroundColor(.red)
+                    
+                    HStack{
+                        Text("Izgara Üzerinde Pişirmeye Uygun")
+                        Spacer()
+                        Text("30 Ocak")
+                    }
+                    
+                    
+                }.padding()
+                
+                VStack(spacing:20){
+                    Text("500 Gram Dana Kıyma")
+                    Text("1 Su bardağı Ekmek")
+                    Text("2 adet soğan")
+                    Text("Yarım Demet Maydanoz")
+                }.padding()
+                
+                Spacer()
+                
+                HStack(spacing:0){
+                    Text("Beğen")
+                        .frame(width:geometry.size.width/2,height: geometry.size.height / 10)
+                        .background(Color.yellow)
+                    Text("Yorum Yap")
+                        .frame(width:geometry.size.width/2,height: geometry.size.height / 10)
+                        .background(Color.orange)
+                }
+            }
+            
+        }
     }
 }
 
